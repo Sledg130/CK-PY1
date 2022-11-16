cb295dd17315133245a02ace04791191857b49ad
Sledg130 <115500564+Sledg130@users.noreply.github.com>
2022-11-16 23:28:02 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -1,9 +1,9 @@
-import random
+from random import randint
 
-def get_unique_list_numbers() -> list[int]:
+def get_unique_list_numbers(min=-10, max=10, len_=15) -> list[int]:
     list_unique_numbers= []
-    while len(list_unique_numbers) < 15: # Задаём ограничения для цикла
-        number = random.randint(-10, 10) # присваиваем каждому числу своё значение
+    while len(list_unique_numbers) < len_: # Задаём ограничения для цикла
+        number = randint(min, max) # присваиваем каждому числу своё значение
         if number not in list_unique_numbers: # Устанавливаем условие уникальности
             list_unique_numbers.append(number) # Добавляем уникальное число
     return list_unique_numbers
@@ -11,4 +11,3 @@ def get_unique_list_numbers() -> list[int]:
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
-
